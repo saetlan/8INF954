82d81dca37c6f096289a576179948ed5491d2a74
Yoann Switala <yoannswitala.etudiant@gmail.com>
2016-10-08 21:35:34 -0400

    Manque derniere question

---
+++ Echantillonneur/Echantillonneur.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class Echantillonneur {
+	
+	public static void help() {
+		System.out.println("java -jar Echantillonneur.jar $Path_BDD $Path_Sortie $Nombre_Echantillonnage $Mode_Verbeux(Optionnel)");
+		System.out.println("$Mode_Verbeux : 0 -> rien(default) | 1 -> avancement");
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		//Variables d'éxécution 
+		int nbEchan = 0;
+		int modeVerbeux = 0;
+		String pathIn = null;
+		String pathOut = null;
+		
+		//Traitement entrées
+		if (args.length < 3 && args.length > 4) {
+			Echantillonneur.help();
+		} else {
+			pathIn = args[0];
+			pathOut = args[1];
+			nbEchan = Integer.parseInt(args[2]);
+			
+			if (args.length == 4) {
+				modeVerbeux = Integer.parseInt(args[3]);
+			}
+		}
+		
+		
+		//Variable de ligne
+		String ligne = null;
+		
+		//Structure de fichier
+		FileWriter out = new FileWriter(pathOut);
+		
+		//Mise en place de l'ecriture sur le fichier
+		BufferedWriter writer = new BufferedWriter(out);
+		
+		//Mise en place de l'entete de fichier
+		BufferedReader reader1 = new BufferedReader(new FileReader(pathIn));
+		for (int i = 0; i < 19; i++) {
+			ligne = reader1.readLine();
+			writer.write(ligne+"\n");
+		}
+		reader1.close();
+		
+		//Borne pour aleatoire
+		int nbLigne = 0;
+		BufferedReader reader2 = new BufferedReader(new FileReader(pathIn));
+		while ( (ligne = reader2.readLine()) != null) {
+			nbLigne++;
+		}		
+		reader2.close();
+		//Echantillonnage aléatoire
+		for (int j = 0; j <= nbEchan; j++) {
+						
+			BufferedReader reader3 = new BufferedReader(new FileReader(pathIn));
+			Random rand = new Random();
+			int nombreAleatoire = rand.nextInt(nbLigne - 19) + 20;
+
+			if (modeVerbeux == 1) {
+				System.out.println("-----------------------");
+				System.out.println("Pioche : "+j);
+				System.out.println("Aléatoire : "+nombreAleatoire);
+				System.out.println("-----------------------");
+			}
+			
+			
+			for (int k = 0; k < nombreAleatoire; k++) {
+				ligne = reader3.readLine();
+			}
+						
+			writer.write(ligne+"\n");
+			reader3.close();
+						
+		}
+		
+		//Close des fichiers
+		writer.close();
+		out.close();
+	}
+
+}
